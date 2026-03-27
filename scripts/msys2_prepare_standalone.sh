3adeb3668d60cd8599ca2b802d598f4262b7a75c
martinakos <martin.tosas@gmail.com>
2026-03-27 19:11:20 +0000

    Fix Windows build: update libopenjph-0.25.dll to -0.26.dll
    OpenJPH bumped SO version in MSYS2.

--- scripts/msys2_prepare_standalone.sh
+++ scripts/msys2_prepare_standalone.sh
@@ -68,7 +68,7 @@ for element_rel in dvipng.exe \
                    libSvtAv1Enc-4.dll \
                    libyuv.dll \
                    libopenjp2-7.dll \
-                   libopenjph-0.25.dll \
+                   libopenjph-0.26.dll \
                    libopenh264-7.dll \
                    libkvazaar-7.dll \
                    libcryptopp.dll
@@ -296,7 +296,7 @@ for element_rel in dvipng.exe \
                    libSvtAv1Enc-4.dll \
                    libyuv.dll \
                    libopenjp2-7.dll \
-                   libopenjph-0.25.dll \
+                   libopenjph-0.26.dll \
                    libopenh264-7.dll \
                    libkvazaar-7.dll \
                    libcryptopp.dll
