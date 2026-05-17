cb013dfbd12965f2f30cd557f5dd816b60876026
martinakos <martin.tosas@gmail.com>
2026-05-17 13:12:07 +0100

    Add horizontal line widget (Insert → Horizontal Line)
    New CtHorizLine anchored widget that draws a full-width 2px line using
    the text foreground color. Stored via filename sentinel __ct_horiz_line
    in the image table, following the same pattern as LaTeX widgets. Supports
    undo/redo, copy/paste, XML/SQLite/MultiFile storage, and HTML/text export.

--- src/ct/ct_export2html.cc
+++ src/ct/ct_export2html.cc
@@ -24,6 +24,7 @@
 #include "ct_export2html.h"
 #include "ct_misc_utils.h"
 #include "ct_main_win.h"
+#include "ct_image.h"
 #include "ct_dialogs.h"
 #include "ct_storage_control.h"
 #include "ct_logging.h"
@@ -113,7 +114,9 @@ void CtExport2Html::node_export_to_html(CtTreeIter tree_iter,
             node_html_text += html_slots[i];
             if (i < widgets.size()) {
                 try {
-                    if (auto embfile = dynamic_cast<CtImageEmbFile*>(widgets[i]))
+                    if (dynamic_cast<CtHorizLine*>(widgets[i]))
+                        node_html_text += "<hr/>";
+                    else if (auto embfile = dynamic_cast<CtImageEmbFile*>(widgets[i]))
                         node_html_text += _get_embfile_html(embfile, tree_iter, _embed_dir);
                     else if (auto image = dynamic_cast<CtImage*>(widgets[i]))
                         node_html_text += _get_image_html(image, _images_dir, images_count, &tree_iter, false/*single_file*/);
@@ -239,7 +242,9 @@ void CtExport2Html::nodes_all_export_to_single_html(bool all_tree, const CtExpor
                 node_html_text += html_slots[i];
                 if (i < widgets.size()) {
                     try {
-                        if (auto embfile = dynamic_cast<CtImageEmbFile*>(widgets[i]))
+                        if (dynamic_cast<CtHorizLine*>(widgets[i]))
+                            node_html_text += "<hr/>";
+                        else if (auto embfile = dynamic_cast<CtImageEmbFile*>(widgets[i]))
                             node_html_text += _get_embfile_html(embfile, tree_iter, _embed_dir);
                         else if (auto image = dynamic_cast<CtImage*>(widgets[i]))
                             node_html_text += _get_image_html(image, _images_dir, images_count, &tree_iter, true/*single_file*/);
@@ -355,7 +360,8 @@ Glib::ustring CtExport2Html::selection_export_to_html(Glib::RefPtr<Gtk::TextBuff
         for (CtAnchoredWidget* widget : widgets) {
             int end_offset = widget->getOffset();
             node_html_text += html_process_slot(_pCtConfig, _pCtMainWin, start_offset, end_offset, text_buffer, false/*single_file*/);
-            if (CtImage* image = dynamic_cast<CtImage*>(widget)) node_html_text += _get_image_html(image, tempFolder, images_count, nullptr, false/*single_file*/);
+            if (dynamic_cast<CtHorizLine*>(widget)) node_html_text += "<hr/>";
+            else if (CtImage* image = dynamic_cast<CtImage*>(widget)) node_html_text += _get_image_html(image, tempFolder, images_count, nullptr, false/*single_file*/);
             else if (auto table = dynamic_cast<CtTableCommon*>(widget)) node_html_text += _get_table_html(table, nullptr, &images_count, false/*single_file*/);
             else if (auto codebox = dynamic_cast<CtCodebox*>(widget)) node_html_text += _get_codebox_html(codebox);
             start_offset = end_offset;
